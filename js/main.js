fc34246ab0c9c8e804130d9ca78265df92072b53
maryfranks <maryfranks@gmail.com>
2017-08-17 17:36:45 -0400

    lighswitch works

--- js/main.js
+++ js/main.js
@@ -1,2 +1,19 @@
 
 
+document.addEventListener('DOMContentLoaded', function() {
+  var mainText = document.querySelector('.status');
+  mainText.innerText = "Turn off the light!";
+
+  var lightswitch = document.querySelector('.switch');
+  lightswitch.addEventListener('click', function(e) {
+    if (lightswitch.className === "switch on") {
+      lightswitch.className = "switch off";
+      document.body.className = "dark";
+      mainText.innerText = "Hey, who turned off the lights?";
+    } else if (lightswitch.className === "switch off") {
+      lightswitch.className = "switch on";
+      document.body.className = "light";
+      mainText.innerText = "It's so bright in here!";
+    }
+  });
+});
